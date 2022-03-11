f1bafd4d4a0e1d35191e58e8b22fb590931177ab
siricco <cco@aon.at>
2022-03-11 21:44:36 +0100

    send_pes_packet(): build as MPEG2 PES packet for better compatibility with several output devices
    Signed-off-by: siricco <cco@aon.at>

--- radioimage.c
+++ radioimage.c
@@ -87,38 +87,42 @@ void cRadioImage::Show(const char *file) {
 
 void cRadioImage::send_pes_packet(unsigned char *data, int len, int timestamp) {
 #define PES_MAX_SIZE 2048
-    int ptslen = timestamp ? 5 : 1;
+    int ptslen = timestamp ? 5 : 0;
     static unsigned char pes_header[PES_MAX_SIZE];
-    pes_header[0] = pes_header[1] = 0;
+    pes_header[0] = 0;
+    pes_header[1] = 0;
     pes_header[2] = 1;
     pes_header[3] = 0xe0;
+    // MPEG-2 PES header
+    pes_header[6] = 0x81;
+    pes_header[7] = ptslen ? 0x80 : 0x0;
+    pes_header[8] = ptslen;
+
+    if (ptslen) {
+        int x;
+        // presentation time stamp:
+        x = (0x02 << 4) | (((timestamp >> 30) & 0x07) << 1) | 1;
+        pes_header[9] = x;
+        x = ((((timestamp >> 15) & 0x7fff) << 1) | 1);
+        pes_header[10] = x >> 8;
+        pes_header[11] = x & 255;
+        x = ((((timestamp) & 0x7fff) << 1) | 1);
+        pes_header[12] = x >> 8;
+        pes_header[13] = x & 255;
+    }
 
     while (len > 0) {
         int payload_size = len;
-        if (6 + ptslen + payload_size > PES_MAX_SIZE) {
-            payload_size = PES_MAX_SIZE - (6 + ptslen);
+        if (6 + 3 + ptslen + payload_size > PES_MAX_SIZE) {
+            payload_size = PES_MAX_SIZE - (6 + 3 + ptslen);
         }
-        pes_header[4] = (ptslen + payload_size) >> 8;
-        pes_header[5] = (ptslen + payload_size) & 255;
-        if (ptslen == 5) {
-            int x;
-            x = (0x02 << 4) | (((timestamp >> 30) & 0x07) << 1) | 1;
-            pes_header[8] = x;
-            x = ((((timestamp >> 15) & 0x7fff) << 1) | 1);
-            pes_header[7] = x >> 8;
-            pes_header[8] = x & 255;
-            x = ((((timestamp) & 0x7fff) < 1) | 1);
-            pes_header[9] = x >> 8;
-            pes_header[10] = x & 255;
-        } else {
-            pes_header[6] = 0x0f;
-        }
+        pes_header[4] = (3 + ptslen + payload_size) >> 8;
+        pes_header[5] = (3 + ptslen + payload_size) & 255;
 
-        memcpy(&pes_header[6 + ptslen], data, payload_size);
-        cDevice::PrimaryDevice()->PlayPes(pes_header, 6 + ptslen + payload_size);
+        memcpy(&pes_header[6 + 3 + ptslen], data, payload_size);
+        cDevice::PrimaryDevice()->PlayPes(pes_header, 6 + 3 + ptslen + payload_size);
         len -= payload_size;
         data += payload_size;
-        ptslen = 1;
     }
 }
 
